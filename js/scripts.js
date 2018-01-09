b6eddfbbcc9b347cccd23e42184520d8746e4310
ivatsu <ivan.tsud@gmail.com>
2018-01-09 22:02:08 +0200

    init project

---
+++ js/scripts.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+	// SLIDERS JS Script 
+	$('#slides').slidesjs({
+		play: {
+          active: true,
+          auto: true,
+          interval: 8000,
+          swap: true
+        }
+  	});
+});
